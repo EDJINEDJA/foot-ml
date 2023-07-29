887a336134971a5424388edd0fbd866ce389e574
EDJINEDJA <edjinedjalaris@gmail.com>
2023-07-29 17:10:26 +0200

    code clean #(0020)

--- src/processed.py
+++ src/processed.py
@@ -4,13 +4,38 @@ from dotenv import load_dotenv
 
 from src.utils.footapi import FOOTAPI
 
+# Load environment variables from the .env file
 load_dotenv("./env/.env")
+
+# Get the API token from the environment variables
 api_token = os.getenv("API_TOKEN")
 
 
 class PROCESSED:
+    """A class for processing competitions using the FOOTAPI."""
+
     def __init__(self, config) -> None:
+        """
+        Initializes a new PROCESSED object.
+
+        Parameters:
+            config (dict): Configuration settings for the FOOTAPI.
+
+        Example usage:
+            config = {"param1": value1, "param2": value2}
+            processed_obj = PROCESSED(config)
+        """
         self.footapi = FOOTAPI(api_token, config)
 
     def competitions(self):
+        """
+        Fetches and returns the list of competitions using the FOOTAPI.
+
+        Returns:
+            list: A list of competition data.
+
+        Example usage:
+            processed_obj = PROCESSED(config)
+            competition_list = processed_obj.competitions()
+        """
         return self.footapi.competitions()
--- src/utils/footapi.py
+++ src/utils/footapi.py
@@ -2,6 +2,22 @@ import requests
 
 
 class FOOTAPI:
+    """
+    A class to interact with the FOOTAPI.
+
+    Parameters:
+        API_TOKEN (str): The API token required for authentication.
+        config (dict): Configuration settings for the API.
+
+    Example usage:
+        config = {
+            "BASE_URL": "https://api.example.com",
+            "COMPETITIONS_URL": "/competitions",
+        }
+        api_token = "your_api_token_here"
+        footapi = FOOTAPI(api_token, config)
+    """
+
     def __init__(self, API_TOKEN, config) -> None:
         self.base_url = config["BASE_URL"]
         self.COMPETITIONS_URL = config["COMPETITIONS_URL"]
@@ -9,12 +25,26 @@ class FOOTAPI:
         self.API_TOKEN = API_TOKEN
         self.headers = {"X-Auth-Token": self.API_TOKEN}
         self.status = requests.get(
-            config["COMPETITIONS_URL"], headers=self.headers
+            self.base_url + config["COMPETITIONS_URL"], headers=self.headers
         ).status_code
         self._auth = self.status == 200
 
     def authentification(funct):
         def wrapped(self, *args, **kwargs):
+            """
+            A decorator to ensure that the method is called only if authenticated.
+
+            Raises:
+                Exception: If the user is not authenticated.
+
+            Returns:
+                The result of the wrapped function if authenticated.
+
+            Example usage:
+                @authentification
+                def some_method(self, *args, **kwargs):
+                    # Method logic here
+            """
             if not self._auth:
                 raise Exception("You must be authenticated to use this method")
             else:
@@ -24,8 +54,21 @@ class FOOTAPI:
 
     @authentification
     def competitions(self):
+        """
+        Fetches and returns the competitions data from the FOOTAPI.
+
+        Returns:
+            dict: A dictionary containing the competitions data.
+
+        Example usage:
+            competitions_data = footapi.competitions()
+        """
         try:
-            response = requests.get(self.COMPETITIONS_URL, self.headers)
-            print(response)
-        except Exception as e:
-            print(f"An error occur: {e}")
+            response = requests.get(
+                self.base_url + self.COMPETITIONS_URL, headers=self.headers
+            )
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return {}
